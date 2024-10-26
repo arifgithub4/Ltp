28569a7c89a0d636cc7666fe5c86957f2455dbbd
arifgithub4 <arifgithub4@yandex.com>
2024-10-26 15:21:47 +0530

    Update 0_ARIF-WALLPAPER.js

--- models/commands/0_ARIF-WALLPAPER.js
+++ models/commands/0_ARIF-WALLPAPER.js
@@ -1,89 +1,30 @@
-const axios = require("axios");
-const fs = require("fs-extra");
-const path = require("path");
-
+////////////////////////////////////////////////////////
+/////// WARNING => JO CREDIT NAME CHANGE KREGA USKA ID BAN KAR DIYA JAYEGA + THIS BOT IS MADE BT ARIF BABU
+const fs = require("fs");
 module.exports.config = {
-  name: "wallpaper",
-  version: "1.0.0", 
-  hasPermssion: 0,
-  credits: ARIF_BABU,
-  description: "THIS BOT WAS MADE BY MR ARIF BABU",
-  commandCategory: "user",
-  usages: "wallpaper", 
-  cooldowns: 5
+        name: "IMOGE 5",
+    version: "1.1.1",
+        hasPermssion: 0,
+        credits: "ARIF BABU", 
+        description: "THIS BOT IS ARIF BABU",
+        commandCategory: "NO PREFIX",
+    cooldowns: 5, 
 };
 
-module.exports.run = async ({ api, event, args }) => {
-  if (args.length === 0) {
-    api.sendMessage("मेरी जान नाम लिखो ♥️😛", event.threadID, event.messageID);
-    return;
+module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
+        var { threadID, messageID } = event;
+        let react = event.body.toLowerCase();
+        if(react.includes("song") ||
+     react.includes("music") || react.includes("audio") || react.includes("play") ||
+react.includes("Song") ||
+react.includes("Music")) {
+                var msg = {
+                                body: `मेरी जान गाने के लिए [ # ] yt लिखो 🎶🙂`,
+                        }
+                        api.sendMessage(msg, threadID, messageID);
+    api.setMessageReaction("🎶", event.messageID, (err) => {}, true)
+                }
+        }
+        module.exports.run = function({ api, event, client, __GLOBAL }) {
+
   }
-
-  const apiKey = "39178311-acadeb32d7e369897e41dba06";
-  const query = encodeURIComponent(args.join(" "));
-  const apiUrl = `https://pixabay.com/api/?key=${apiKey}&q=${query}&image_type=photo&per_page=200`;
-
-  try {
-    const response = await axios.get(apiUrl);
-    const wallpapers = response.data.hits.filter(wallpaper => {
-      const imageUrl = wallpaper.largeImageURL;
-      const imageExtension = path.extname(imageUrl);
-      return imageExtension === ".jpg" || imageExtension === ".png";
-    });
-
-    if (wallpapers.length === 0) {
-      api.sendMessage("मेरी जान ये नहीं है 😌❌", event.threadID, event.messageID);
-      return;
-    }
-
-    let streams = [];
-    let counter = 0;
-
-    for (const wallpaper of wallpapers) {
-      if (counter >= 10) {
-        break;
-      }
-
-      const imageUrl = wallpaper.largeImageURL;
-      const imageExtension = path.extname(imageUrl);
-
-      let imagePath = path.join(__dirname, `/cache/wallpaper${counter}${imageExtension}`);
-      let hasError = false;
-
-      try {
-        const imageResponse = await axios.get(imageUrl, { responseType: "arraybuffer" });
-        fs.writeFileSync(imagePath, Buffer.from(imageResponse.data, "binary"));
-      } catch (error) {
-        console.error(error);
-        hasError = true;
-      }
-
-      if (!hasError) {
-        streams.push(fs.createReadStream(imagePath).on("end", () => {
-          if (fs.existsSync(imagePath)) {
-            fs.unlink(imagePath, err => {
-              if (err) console.error(err);
-            });
-          }
-        }));
-
-        counter += 1;
-      }
-    }
-
-    if (streams.length > 0) {
-      let msg = {
-        body: `𝐂𝐑𝐄𝐀𝐓𝐄𝐑 𝐀𝐑𝐈𝐅 𝐁𝐀𝐁𝐔 ♥️`,
-        attachment: streams
-      };
-
-      api.sendMessage(msg, event.threadID, event.messageID);
-    } else {
-      api.sendMessage("An error occurred while fetching the wallpapers.", event.threadID, event.messageID);
-    }
-
-  } catch (error) {
-    console.error(error);
-    api.sendMessage("An error occurred while fetching wallpapers.", event.threadID, event.messageID);
-  }
-};
